e9c5b85f828bd8a6f42f5208afedac3658e376c2
Blane <161300198+Blane187@users.noreply.github.com>
2024-03-22 23:56:33 +0700

    Update inference.py

--- tabs/inference/inference.py
+++ tabs/inference/inference.py
@@ -239,11 +239,22 @@ def inference_tab():
                 )
 
             with gr.Row():
-                url = gr.Textbox(label="url to yotube link.")
-                    audio_name = gr.Textbox(label="file name.")
-                    dwnl_button = gr.Button("Download")
-                    dwnl_button.click(fn=download_audio,inputs=[audio_name],outputs=[url])
-
+                url = gr.Textbox(
+                    label=("url to yotube link"),
+                )
+                audio_name = gr.Textbox(
+                    label=("audio name"),
+                )
+                dwnl_button = gr.Textbox(
+                    label=("download button"),
+                )
+                dwnl_button.click(
+                    fn=download_audio,
+                    inputs=[audio_name],
+                    outputs=[url],
+                )
+                
+                    
         with gr.Accordion(i18n("Advanced Settings"), open=False):
             with gr.Column():
                 clear_outputs_infer = gr.Button(
